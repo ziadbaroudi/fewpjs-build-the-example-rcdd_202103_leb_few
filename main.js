f6dd5362930e2584637eb74a668106ca6899e0c0
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-28 00:10:32 +0000

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
-
+let heart = document.getElementById("heart");
+console.log(heart)
+let m = document.getElementById("modal");
+function like(ev){
+  let specific_heart = ev.target
+  mimicServerCall()
+  .then(function(){
+    if(specific_heart.innerHTML == EMPTY_HEART){
+    specific_heart.innerText = FULL_HEART;
+    specific_heart.classList.add("activated-heart")
+    }
+    else{
+      specific_heart.innerText = EMPTY_HEART;
+      specific_heart.className = "";
+    }
+  })
+  .catch(function(me){
+    m.innerText=me;
+    m.className =""
+    setTimeout(()=>m.className="hidden",3000);
+  });
+}
+let hearts = document.querySelectorAll(".like-glyph");
+for(let i of hearts) {
+  i.addEventListener("click", like)
+}
 
 
 
